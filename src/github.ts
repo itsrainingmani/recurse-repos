6c047fbb69d121a7ce1fc95180b1ff31109ee492
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-02-27 15:41:09 -0500

    refactor: move common functions into files

---
+++ src/github.ts
@@ -0,0 +1,124 @@
+import { graphql, GraphqlResponseError } from '@octokit/graphql';
+import { Octokit } from 'octokit';
+import type { Member } from './utils';
+
+const octokit = new Octokit({
+	auth: process.env['GITHUB_TOKEN'],
+});
+
+async function getGithubOrgUsers() {
+	try {
+		let iterator = octokit.paginate.iterator(
+			'GET /orgs/recursecenter/members',
+			{
+				org: 'ORG',
+				per_page: 100,
+				headers: {
+					'X-GitHub-Api-Version': '2022-11-28',
+				},
+			}
+		);
+
+		let memberData: any = [];
+		let breakLoop = false;
+		for await (const { data } of iterator) {
+			if (breakLoop) break;
+			console.log(...data);
+			memberData = [...memberData, ...data];
+		}
+
+		const path = '.data/members.json';
+		const bytes = await Bun.write(path, JSON.stringify(memberData));
+		console.log(`${bytes} bytes written to file`);
+	} catch (error: any) {
+		if (error.response) {
+			console.error(
+				`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`
+			);
+		}
+		console.error(error);
+	}
+}
+
+async function getTop50repos() {
+	const members_file = Bun.file('../data/members.json');
+	let rc_members: Array<Member> = await members_file.json();
+	let list_rc_members = rc_members.map((member) => member.login);
+
+	let query = `
+query listTopRepos($queryString: String!) {
+  rateLimit {
+    cost
+    remaining
+    resetAt
+  }
+  search(query: $queryString, type: USER, first: 1) {
+    nodes {
+      ... on RepositoryOwner {
+        repositories(
+          first: 30
+          privacy: PUBLIC
+          isArchived: false
+          ownerAffiliations: OWNER
+          orderBy: { field: STARGAZERS, direction: DESC }
+        ) {
+          nodes {
+            name
+            owner {
+              login
+            }
+            description
+            stargazerCount
+            createdAt
+            updatedAt
+            isFork
+            url
+            object(expression: "HEAD:README.md") {
+              ... on Blob {
+                text
+              }
+            }
+            languages(
+              first: 5
+              orderBy: { field: SIZE, direction: DESC }
+            ) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+	let all_repos: any[] = [];
+
+	try {
+		for (let member of list_rc_members) {
+			console.log(`Current user - ${member}`);
+			const result: any = await octokit.graphql(query, {
+				queryString: `user:${member}`,
+			});
+
+			if (result !== undefined) {
+				let search_results: any[] = result['search']['nodes'];
+				if (search_results.length > 0) {
+					let repos = result['search']['nodes'][0]['repositories']['nodes'];
+					all_repos.push({ user: member, repos: repos });
+				}
+			}
+		}
+
+		Bun.write('../data/all_repos.json', JSON.stringify(all_repos));
+	} catch (error) {
+		if (error instanceof GraphqlResponseError) {
+			console.error(
+				`Request Failed: ${error.request} with message: ${error.message}`
+			);
+		} else {
+			console.error(error);
+		}
+	}
+}
--- src/recurse.ts
+++ src/recurse.ts
@@ -1,51 +1,117 @@
-import type { Member } from "./utils";
-
-const members_file = Bun.file("./data/members.json");
-let rc_members: Array<Member> = await members_file.json();
-let list_rc_members = rc_members.map((member) => member.login);
-
-function is_required(name: string) {
-  throw new Error(`Argument, \`${name}\`, is required.`);
-}
+import {
+	constructMetadataString,
+	type Member,
+	type Repo,
+	type UserRepos,
+} from './utils';
 
 const opts = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.RC_TOKEN}`,
-  },
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${process.env.RC_TOKEN}`,
+	},
 };
 
-console.log(opts);
-
 async function paginated_fetch(
-  url = is_required("url"), // Improvised required argument in JS
-  offset = 0,
-  previousResponse = [],
-) {
-  return fetch(`${url}?role=recurser&limit=50&offset=${offset}`, opts) // Append the page number to the base URL
-    .then((response) => response.json())
-    .then((newResponse) => {
-      const response = [...previousResponse, ...newResponse]; // Combine the two arrays
+	url: string, // Improvised required argument in JS
+	offset = 0,
+	previousResponse: any[] = []
+): Promise<any[]> {
+	return fetch(`${url}?role=recurser&limit=50&offset=${offset}`, opts) // Append the page number to the base URL
+		.then((response) => response.json())
+		.then((newResponse: any) => {
+			const response = [...previousResponse, ...newResponse]; // Combine the two arrays
 
-      if (newResponse.length !== 0) {
-        offset += newResponse.length;
-        console.log(`Collected ${offset} profiles`);
-        return paginated_fetch(url, offset, response);
-      }
+			if (newResponse.length !== 0) {
+				offset += newResponse.length;
+				console.log(`Collected ${offset} profiles`);
+				return paginated_fetch(url, offset, response);
+			}
 
-      return response;
-    });
+			return response;
+		});
 }
 
-try {
-  console.log("Getting Recurse profiles");
-  let profiles = await paginated_fetch(
-    `https://www.recurse.com/api/v1/profiles`,
-  );
+async function getAllRecursers() {
+	const members_file = Bun.file('../data/members.json');
+	let rc_members: Array<Member> = await members_file.json();
+	let list_rc_members = rc_members.map((member) => member.login);
 
-  console.log(`Got ${profiles.length} profiles`);
-  Bun.write("./data/recursers.json", JSON.stringify(profiles));
-} catch (e) {
-  console.error(e);
+	try {
+		console.log('Getting Recurse profiles');
+		let profiles = await paginated_fetch(
+			`https://www.recurse.com/api/v1/profiles`
+		);
+
+		console.log(`Got ${profiles.length} profiles`);
+		Bun.write('../data/recursers.json', JSON.stringify(profiles));
+	} catch (e) {
+		console.error(e);
+	}
 }
+
+async function recursersByStint() {
+	const recursers_file = Bun.file('../data/recursers.json');
+	let rc_members: Array<any> = await recursers_file.json();
+
+	const members_file = Bun.file('../data/members.json');
+	let github_members: Array<Member> = await members_file.json();
+
+	// filter out whatever github usernames aren't present
+	rc_members = rc_members.filter((rc) => rc.github !== null);
+
+	for (let recurser of rc_members) {
+		console.log(recurser.github);
+
+		if (recurser.stints.length > 0) {
+			for (let stint of recurser.stints) {
+				console.log(
+					`Started on ${stint.start_date} and ended on ${stint.end_date}`
+				);
+			}
+		}
+	}
+}
+
+async function printRepoNamesAndOwners() {
+	const repoFile = Bun.file('../data/all_repos_partial.json');
+	const data: UserRepos[] = await repoFile.json();
+
+	let total_repos = 0;
+	for (const datum of data.slice(0, 5)) {
+		console.log(datum.user);
+		total_repos += datum.repos.length;
+		for (const repo of datum.repos.slice(0, 1)) {
+			let metadata = constructMetadataString(repo);
+			console.log(metadata);
+		}
+	}
+
+	console.log(total_repos);
+}
+
+async function constructReadmeData() {
+	const repoFile = Bun.file('../data/all_repos_partial.json');
+	const data: UserRepos[] = await repoFile.json();
+
+	const readmeFile = Bun.file('../data/readme-data.txt');
+	const writer = readmeFile.writer();
+	let total_repos = 0;
+	for (const datum of data) {
+		console.log(datum.user);
+
+		for (const repo of datum.repos) {
+			let metadata = constructMetadataString(repo);
+			let repoReadme = repo.object?.text;
+
+			if (repoReadme !== undefined) {
+				metadata = '\n\n' + metadata + '\n' + repoReadme + '\n\n';
+			}
+
+			writer.write(metadata);
+		}
+	}
+}
+
+// constructReadmeData();
